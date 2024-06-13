9a9694f283be734744ec05822d1f2119555a0986
Alex <alexpartington1@gmail.com>
2024-06-13 17:41:50 -0400

    initial save

---
+++ src/components/JokeComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="app">
+      <h1>Random Joke</h1>
+      <button @click="getJoke">Get a Joke</button>
+      <p v-if="joke">{{ joke }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        joke: ''
+      };
+    },
+    methods: {
+      async getJoke() {
+        try {
+          const response = await axios.get('https://api.humorapi.com/jokes/random', {
+            headers: {
+              'x-api-key': 'bd0b86b9a80d416098266562654acf14'
+            }
+          });
+          this.joke = response.data.joke;
+        } catch (error) {
+          console.error('Error fetching joke:', error);
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  #app {
+    text-align: center;
+    margin-top: 50px;
+  }
+  button {
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+  </style>
+  
